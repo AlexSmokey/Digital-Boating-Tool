0139b5320f3603854505e73c05a7cdd58fa77fdc
Alex Goebel <goebelag@rose-hulman.edu>
2017-01-15 22:51:23 -0500

    can now go from mainactivity to breadcrumbs

--- FinalProject/app/src/main/java/edu/rose_hulman/humphrjm/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/edu/rose_hulman/humphrjm/finalproject/MainActivity.java
@@ -1,15 +1,17 @@
 package edu.rose_hulman.humphrjm.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE_INPUT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        RelativeLayout breadCrumbsButton = (RelativeLayout) findViewById(R.id.breadCrumbs);
+
+        breadCrumbsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openBreadCrumbs();
+            }
+        });
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +44,11 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    private void openBreadCrumbs() {
+        Intent MainIntent = new Intent(this, BreadCrumbsActivity.class);
+        startActivityForResult(MainIntent, REQUEST_CODE_INPUT);
     }
 
     @Override
