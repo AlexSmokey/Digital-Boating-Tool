072ea446e3692109b6b899803cd590406b8665b9
Alex Goebel <goebelag@rose-hulman.edu>
2017-01-15 22:52:48 -0500

    TEMPORARY can now go from breadcrumbs to main activity

--- FinalProject/app/src/main/java/edu/rose_hulman/humphrjm/finalproject/Splashscreen.java
+++ FinalProject/app/src/main/java/edu/rose_hulman/humphrjm/finalproject/Splashscreen.java
@@ -1,8 +1,11 @@
 package edu.rose_hulman.humphrjm.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by humphrjm on 12/18/2016.
@@ -10,8 +13,23 @@ import android.support.v7.app.AppCompatActivity;
 
 public class Splashscreen extends AppCompatActivity {
 
+    public static final int REQUEST_CODE_INPUT = 1;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+        final ImageView imageView = (ImageView)this.findViewById(R.id.image);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openApp();
+            }
+        });
+    }
+
+    private void openApp() {
+        Intent MainIntent = new Intent(this, MainActivity.class);
+        startActivityForResult(MainIntent, REQUEST_CODE_INPUT);
     }
 }
