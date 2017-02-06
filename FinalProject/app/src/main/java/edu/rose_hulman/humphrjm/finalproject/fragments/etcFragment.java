9a47a340465427b86b952097d203d7de1515f14b
Alex Goebel <goebelag@rose-hulman.edu>
2017-02-06 12:16:10 -0500

    added message to direct user

--- FinalProject/app/src/main/java/edu/rose_hulman/humphrjm/finalproject/fragments/etcFragment.java
+++ FinalProject/app/src/main/java/edu/rose_hulman/humphrjm/finalproject/fragments/etcFragment.java
@@ -3,29 +3,38 @@ package edu.rose_hulman.humphrjm.finalproject.fragments;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import edu.rose_hulman.humphrjm.finalproject.R;
 
-public class etcFragment extends Fragment {
+public class etcFragment extends Fragment implements View.OnClickListener {
 
     public etcFragment(){}
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.etc_info, container, false);
+        TextView mText = (TextView) view.findViewById(R.id.textView);
+        mText.setOnClickListener(this);
+        ImageView mImage = (ImageView) view.findViewById(R.id.imageView);
+        mImage.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Snackbar.make(getView(), "Hit back to choose another feature", Snackbar.LENGTH_LONG).show();
     }
 }
